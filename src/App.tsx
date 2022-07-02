48b3c88c364c6b2837a47ea244ad6eeeefccf154
Jack Davey <johncdavey2000@gmail.com>
2022-07-01 20:34:02 -0700

    Abstract animated interface elements added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { motion, useCycle } from 'framer-motion';
 
 function App() {
 
-  const variants = {
+  const blockVariants = {
     active: {
       backgroundColor: "#f00",
       borderRadius: "50%",
@@ -17,18 +17,35 @@ function App() {
     }
   }
 
+  const containerVariants = {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+      transition: { duration: 0.5 }
+    },
+    visible: {
+      scale: 1,
+      opacity: 1,
+      transition: { duration: 0.5 }
+    }
+  }
+
   const [isActive, cycle] = useCycle("inactive", "active");
+  const [containerHidden, setContainerHidden] = useCycle('visible', 'hidden');
 
   return (
-    <div id='animContainer'>
-      <motion.div
-        className='animBlock'
-        animate={{ rotate: 360, borderRadius: '50%', backgroundColor: '#f00' }}
-        // whileHover={{ scale: 1.5, rotate: 0, borderRadius: '0%', backgroundColor: '#fff' }}
-        transition={{ duration: 2, repeat: Infinity, repeatType: 'mirror', ease: 'anticipate' }}
-      />
-      <motion.div variants={variants} className='animBlock' animate={isActive} onClick={() => cycle()} />
-    </div>
+    <>
+      <div id='toggleBtn' onClick={() => setContainerHidden()} />
+      <motion.div id='animContainer' variants={containerVariants} animate={containerHidden}>
+        <motion.div
+          className='animBlock'
+          animate={{ rotate: 360, borderRadius: '50%', backgroundColor: '#f00' }}
+          // whileHover={{ scale: 1.5, rotate: 0, borderRadius: '0%', backgroundColor: '#fff' }}
+          transition={{ duration: 2, repeat: Infinity, repeatType: 'mirror', ease: 'anticipate' }}
+        />
+        <motion.div variants={blockVariants} className='animBlock' animate={isActive} onClick={() => cycle()} />
+      </motion.div>
+    </>
   );
 }
 
